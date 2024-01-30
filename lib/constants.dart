e4dc6f8697197ae3a0a1ba8119f291323ce3ca79
Umberto Colangelo <umberto.colangelo@mail.polimi.it>
2024-01-30 03:50:14 +0100

    modified:   lib/constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,1 @@
 const String googleMapApiKey = "AIzaSyAi174qroDFd3IVtZGUbfB4THSvZUC9DmM";
-
-const String key = "ck_3148de4b1e8752810b7e792692a82e5f694ca984";
-const String secret = "cs_48ef07d29108ea78cd97d77b888bc2d7c3e97985";
-const String paypalClientID =
-    "AYYhbgihgcEMz7vsDR4xdzlIYOTqurHq5aB0U6nHqe6G8hfuaUM4lN94ImY_h3Jg2or7BZM44u8LX8WJ";
-const String paypalSecretKey =
-    "EFX24oLMAQH6suXP9oTY2opoLhXO7PiZyxx3TR7mrXfg-jmN2pbfNZFL98GispoBWczh6U-pudMAXjSM";
-const String paypalURL = "https://api.sandbox.paypal.com"; //for sandbox mode
-//const String paypalURL = "https://api.paypal.com"; //for Production mode
-const String orderURL = "/wp-json/wc/v2/orders";
-const String currency = "€";
